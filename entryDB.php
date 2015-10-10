edb205f76faac36d0ada86099a418ffcb1ca1fc1
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-10-10 12:10:31 +0530

    Create entryDB.php

---
+++ entryDB.php
@@ -0,0 +1,11 @@
+<?php
+
+$con=mysql_connect('mysql2.000webhost.com','a9431695_sanchit','sanchitapril1994')or die("cannot connect");;
+mysql_select_db("a9431695_insert" , $con);
+
+$message=$_POST['message'];
+
+mysql_query("insert into insert(message) values('$message')");
+
+mysql_close($con);
+?>
