ce293d1cbcafcce6df8c400ec71183436bf85ed5
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-10-10 12:16:07 +0530

    Update entryDB.php

--- entryDB.php
+++ entryDB.php
@@ -3,7 +3,9 @@
 $con=mysql_connect('Host_Name','User_Name','Password')or die("cannot connect");;
 mysql_select_db("Database_Name" , $con);
 
-$message=$_POST['message'];
+$name=$_POST['name'];
+$age =$_POST['age'];
+$mobile = $_POST['mobile'];
 
 mysql_query("insert into insert(message) values('$message')");
 
