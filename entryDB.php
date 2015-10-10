8f8888cab1f367c983984520af56985e6a278ab0
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-10-10 12:12:16 +0530

    Update entryDB.php

--- entryDB.php
+++ entryDB.php
@@ -1,7 +1,7 @@
 <?php
 
 $con=mysql_connect('mysql2.000webhost.com','','')or die("cannot connect");;
-mysql_select_db("a9431695_insert" , $con);
+mysql_select_db("" , $con);
 
 $message=$_POST['message'];
 
