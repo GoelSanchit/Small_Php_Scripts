2a2c2a8a3374f28da1904546b7ca965be2f55b07
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-11-09 11:12:58 +0530

    Create ziu_boys.php

---
+++ ziu_boys.php
@@ -0,0 +1,23 @@
+<?php
+// Note: Replace 'api-eu' with 'api-us', if you are connecting to US cloud!
+$url = 'https://api-us.clusterpoint.com/v4/101893/todo';
+$userPassword = 'username:password';
+$ch = curl_init();
+$query = 'SELECT * FROM todo';
+curl_setopt($ch, CURLOPT_URL, $url . '/_query');
+curl_setopt($ch, CURLOPT_USERPWD, $userPassword);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+$response = json_decode(curl_exec($ch));
+$errorMsg = curl_error($ch);
+if ($errorMsg) {
+	var_dump($errorMsg);
+}
+curl_close($ch);
+
+echo json_encode($response->results);
+
+?>
