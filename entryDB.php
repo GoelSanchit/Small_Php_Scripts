4cd464313f0c331a9c45ee4239651c54eea66a2a
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-12-15 19:04:13 +0530

    Update entryDB.php

--- entryDB.php
+++ entryDB.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysql_connect('Host_Name','User_Name','Password')or die("cannot connect");;
+$con=mysql_connect('Host_Name','User_Name','Password')or die("cannot connect");
 mysql_select_db("Database_Name" , $con);
 
 $name=$_POST['name'];
